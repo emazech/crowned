60748e313c1d4ca4a84099984f4b888c73787d98
Elonita Krasniqi <elonitakrasniqi@gmail.com>
2024-03-30 16:34:20 +0100

    Completion of the design for the starter components!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-import Login from './Login'
+import Login from './Login';
+import Crown from './assets/crown.png';
+import Icon from './assets/icon.png';
 
 function App() {
   return (
     <div className="App">
+      <div className="side">
+      <img src={Crown} alt='CrownIcon' className='crown-icon' />
+      <p className='crown'>WELCOME TO CROWNED</p>
+        <img src={Icon} alt='GameIcon'></img>
+      </div>
       <Login />
     </div>
   );
