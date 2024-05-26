14b2a36b47dcd60a11265e068548df1bfdb477ad
emazechiri <ema.zeqiri@student.uni-pr.edu>
2024-05-26 20:15:34 +0200

    axios trying

---
+++ src/axiosInstance.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // You can add custom headers or tokens here if needed
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Handle errors
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
