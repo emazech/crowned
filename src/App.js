15811b97b365bd0baac353eaedc8732689bcbcce
ema <ema.zeqiri@student.uni-pr.edu>
2024-05-27 20:07:55 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import React, { useState } from "react";
-import "./App.css";
-import CategoryAndNumOfLettersToChoose from "./SaveTheCheese/Components/CategoryAndNumOfLetterToChoose.js";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import WordToGuess from "./SaveTheCheese/Components/WordToGuess.js";
-import Maze from "./Maze/Maze.js";
-import Crown from "./assets/crown.png";
-import Icon from "./assets/icon.png";
-import Home from "./Quiz/pages/Home.jsx";
-import React from 'react';
-import Login from './Login';
-import SignUp from './SignUp';
-import CoinCollector from './CoinCollector';
-import Snake from './snake';
-import TimedCC from './TimedCC';
-import TimeCoinCollector from './TimeCoinCollector';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import ChooseGame from "./ChooseGames";
+import CoinCollector from "./CoinCollector";
+import Snake from "./Snake";
+import ChainsawMaze from "./Maze/Maze";
+import TriviaQuestions from "./Quiz/pages/Home";
+import MemoryCard from "./Memory/Memory";
+import SaveTheCheese from "./SaveTheCheese/Components/WordToGuess";
+import CategoryAndNumOfLettersToChoose from "./SaveTheCheese/Components/CategoryAndNumOfLetterToChoose";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    handleLogin();
+  }, []);
+
+  const handleLogin = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    setUser(storedUser);
+  };
+
   return (
+
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/coin" element={<CoinCollector />} />
-        <Route path="/snake" element={<Snake />} />
-        <Route path="/TimedCC" element={<TimedCC />} />
-        <Route path="/timecoin" element={<TimeCoinCollector />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+      <div>
+        <Routes>
+          {!user && (
+            <>
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="/signup" element={<SignUp />} />
+            </>
+          )}
+          {user && <Route path="/choose-game" element={<ChooseGame />} />}
+          <Route path="/coin" element={<CoinCollector />} />
+          <Route path="/snake" element={<Snake />} />
+          <Route path="/chainsaw-maze" element={<ChainsawMaze />} />
+          <Route path="/save-the-cheese" element={<SaveTheCheese />} />
+          <Route path="/trivia-questions" element={<TriviaQuestions />} />
+          <Route path="/memory-card" element={<MemoryCard />} />
+          {user ? (
+            <>
+              <Route path="/" element={<CategoryAndNumOfLettersToChoose />} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="/choose-game" />} />
+          )}
+        </Routes>
+      </div>
+      </Router>
   );
 }
 
