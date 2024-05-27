4cf973e4381a667a593db339c1685a95c846ee7c
eramaliqi <122036080+eramaliqi@users.noreply.github.com>
2024-05-27 18:02:00 +0200

    Routes Fixing
    Not Complete!

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
 import React, { useState } from "react";
 import "./App.css";
-import Login from "./Login";
-import SignUp from "./SignUp";
 import CategoryAndNumOfLettersToChoose from "./SaveTheCheese/Components/CategoryAndNumOfLetterToChoose.js";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import WordToGuess from "./SaveTheCheese/Components/WordToGuess.js";
 import Maze from "./Maze/Maze.js";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Crown from "./assets/crown.png";
 import Icon from "./assets/icon.png";
 import Home from "./Quiz/pages/Home.jsx";
+import React from 'react';
+import Login from './Login';
+import SignUp from './SignUp';
+import CoinCollector from './CoinCollector';
+import Snake from './snake';
+import TimedCC from './TimedCC';
+import TimeCoinCollector from './TimeCoinCollector';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleSignUp = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
-    <div>
-      <div className="side">
-        <img src={Crown} alt="CrownIcon" className="crown-icon" />
-        <p className="crown">WELCOME TO CROWNED</p>
-        <img src={Icon} alt="GameIcon" />
-      </div>
-      {/* <Maze /> */}
-      <HashRouter>
-        <Routes>
-          <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          <Route path="/signup" element={<SignUp onSignUp={handleSignUp} />} />
-          {isLoggedIn ? (
-            <>
-              <Route path="/game" element={<WordToGuess />} />
-              <Route path="/" element={<CategoryAndNumOfLettersToChoose />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="*" element={<Navigate to="/home" />} />
-            </>
-          ) : (
-            <Route path="*" element={<Navigate to="/login" />} />
-          )}
-        </Routes>
-      </HashRouter>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/coin" element={<CoinCollector />} />
+        <Route path="/snake" element={<Snake />} />
+        <Route path="/TimedCC" element={<TimedCC />} />
+        <Route path="/timecoin" element={<TimeCoinCollector />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
